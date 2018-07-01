db3f7e64781a7e9516a10b0b2aca578e41a7a97b
Rahul <k.dasgupta112@gmail.com>
2018-07-02 07:02:18 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <v-app dark>
+      <!-- <div class="login-container"> -->
+        <login/>
+      <!-- </div> -->
+    </v-app>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import login from './components/Login'
+export default {
+  name: 'App',
+  components: {
+    login
+  }
+}
+</script>
+
+<style>
+#image_id{
+  height:10px;
+  width:10px;
+}
+.login-container{
+  background-color: black;
+  margin:0;
+}
+#app {
+  font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color:white;
+  height: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+      <v-container app align-baseline>
+          <div class="form-ui">
+            <h1>POTOROO</h1>
+            <v-form action="" class="login-form">
+                    <v-text-field
+                        label='User Name'
+                    ></v-text-field>
+                    <v-text-field
+                    label='Password'
+                    :type="'Password'"
+                    ></v-text-field>
+                <div class="input-div">
+                    <v-flex class="align-content-space-around">
+                        <v-btn outline type="submit">Login</v-btn>
+                    </v-flex>
+                </div>
+            </v-form>
+          </div>
+      </v-container>
+    <!-- <div></div> -->
+</template>
+<script>
+export default {
+  name: 'login'
+}
+</script>
+<style>
+.data-app{
+    margin:0;
+}
+.form-ui{
+    position:relative;
+    top:30%;
+    overflow: hidden;
+    height: 100%;
+}
+.login-form{
+    padding-top:5px;
+}
+.input-div{
+    padding-top:5px;
+}
+.input-div button{
+    color:grey;
+    background-color:white;
+    align-content:center;
+}
+input,button{
+    padding-top:5px;
+    width:50%;
+}
+</style>
